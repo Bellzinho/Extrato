5684d522b9d92b7c8449ea2810dc088557c22448
Bellzinho <edgarttister@gmail.com>
2020-05-11 18:50:53 -0300

    Adição de saldo inicial

--- Extrato/Extrato/Calculo.cs
+++ Extrato/Extrato/Calculo.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Extrato
 {
     class Calculo
     {
+        //  VARIAVEIS
+        private double Saldo;
+        public double Entrada { get; private set; }
+        public double Saida { get; private set; }
+
+        //  METODOS
+        public double GetSaldo()
+        {
+            return Saldo;
+        }
+        public void SetSaldo(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Credito(double entrada)
+        {
+            Saldo += entrada;
+        }
+        public void Debito(double saida)
+        {
+            Saldo -= saida;
+        }
     }
 }
--- Extrato/Extrato/Program.cs
+++ Extrato/Extrato/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+
 
 namespace Extrato
 {
@@ -10,6 +12,30 @@ namespace Extrato
     {
         static void Main(string[] args)
         {
+            Calculo extrato = new Calculo();
+            double saldo;
+
+            Console.WriteLine("Possui saldo inicial? \nS: Para digitar o saldo! \nN: Para prosseguir!");
+            char resp = char.Parse(Console.ReadLine());
+            switch (resp)
+            {
+                case 'S':
+                    Console.Write("Digite o saldo inicial: ");
+                    saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    extrato.SetSaldo(saldo);
+                    break;
+                case 'N' :
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Erro 001: Feche o programa e tente novamente!\n");
+                    break;
+            }
+
+            Console.WriteLine("Escolha o tipo de operção que foi feita! \nC: Para crédito! \nD: Para débito!");
+
+            
+
         }
     }
 }
